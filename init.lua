6b91b002271a1b24ca087518d15f7895819bcfc1
Sam Mitchell <jfetkotto@gmail.com>
2022-04-03 14:18:43 +0100

    working base

--- init.lua
+++ init.lua
@@ -2,53 +2,201 @@
 -- samuel.mitchell@nordicsemi.no
 -- Jan 2022
 
-require ('base') -- Basic Vim settings
-require ('keymap')
-require ('filetypes')
-
-
 require "paq" {
         "savq/paq-nvim";
         "nvim-lualine/lualine.nvim";
-        "terryma/vim-multiple-cursors";
         "neovim/nvim-lspconfig";
         "preservim/nerdtree";
         "jiangmiao/auto-pairs";
-        "airblade/vim-gitgutter";
         "hrsh7th/nvim-cmp";
         "hrsh7th/cmp-nvim-lsp";
         "hrsh7th/cmp-buffer";
         "hrsh7th/cmp-path";
-        "hrsh7th/cmp-cmdline";
-        "hrsh7th/cmp-vsnip";
-        "hrsh7th/vim-vsnip";
+        "saadparwaiz1/cmp_luasnip";
+        "L3MON4D3/LuaSnip";
         "ntpeters/vim-better-whitespace";
+        "lewis6991/gitsigns.nvim";
+        "numToStr/Comment.nvim";
         -- Colourschemes
-        "folke/tokyonight.nvim";
-        "junegunn/seoul256.vim";
         "nanotech/jellybeans.vim";
 }
+
+--Set highlight on search
+vim.o.hlsearch = false
+--Make line numbers default
+vim.wo.number = true
+--Enable mouse mode
+vim.o.mouse = 'a'
+--Enable break indent
+vim.o.breakindent = true
+--Save undo history
+vim.opt.undofile = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+--Decrease update time
+vim.o.updatetime = 250
+vim.wo.signcolumn = 'yes'
+
+vim.o.cursorline = true
+vim.o.colorcolumn = "80"
+vim.o.clipboard = "unnamedplus"
+vim.o.backup = false
+vim.o.swapfile = false
+vim.o.writebackup = false
+vim.o.splitright = true
+vim.o.splitbelow = true
+
+vim.o.tabstop = 2
+vim.bo.tabstop = 2
+vim.o.softtabstop = 2
+vim.bo.softtabstop = 2
+vim.o.shiftwidth = 2
+vim.bo.shiftwidth = 2
+vim.o.autoindent = true
+vim.bo.autoindent = true
+vim.o.expandtab = true
+vim.bo.expandtab = true
+
+-- Disable various builtin plugins in Vim that bog down speed
+vim.g.loaded_matchparen = 1
+vim.g.loaded_matchit = 1
+vim.g.loaded_logiPat = 1
+vim.g.loaded_rrhelper = 1
+vim.g.loaded_tarPlugin = 1
+vim.g.loaded_gzip = 1
+vim.g.loaded_zipPlugin = 1
+vim.g.loaded_2html_plugin = 1
+vim.g.loaded_shada_plugin = 1
+vim.g.loaded_spellfile_plugin = 1
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_tutor_mode_plugin = 1
+vim.g.loaded_remote_plugins = 1
+
+--Set colorscheme
+vim.o.termguicolors = true
+vim.g.rainbow_active = 1
+vim.cmd[[colorscheme jellybeans]]
+-- Set completeopt to have a better completion experience
+vim.o.completeopt = 'menuone,noselect'
+
+-- Space as leader
+vim.api.nvim_set_keymap('', '<Space>', '<Nop>', { noremap = true, silent = true })
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.api.nvim_set_keymap('n', '<leader>w', ':bn<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<leader>q', ':bdelete<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<C-f>', ':NERDTreeToggle<CR>', {noremap = true})
+
+-- Highlight on yank
+vim.cmd [[
+  augroup YankHighlight
+    autocmd!
+    autocmd TextYankPost * silent! lua vim.highlight.on_yank()
+  augroup end
+]]
+
+--Map blankline
+vim.g.indent_blankline_char = '┊'
+vim.g.indent_blankline_filetype_exclude = { 'help', 'packer' }
+vim.g.indent_blankline_buftype_exclude = { 'terminal', 'nofile' }
+vim.g.indent_blankline_show_trailing_blankline_indent = false
+
+-- Gitsigns
+require('gitsigns').setup {
+  signs = {
+    add = { text = '+' },
+    change = { text = '~' },
+    delete = { text = '_' },
+    topdelete = { text = '‾' },
+    changedelete = { text = '~' },
+  },
+}
+
 -- LSPs
+local lspconfig = require 'lspconfig'
+local on_attach = function(_, bufnr)
+  local opts = { noremap = true, silent = true }
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>so', [[<cmd>lua require('telescope.builtin').lsp_document_symbols()<CR>]], opts)
+  vim.cmd [[ command! Format execute 'lua vim.lsp.buf.formatting()' ]]
+end
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+
 local servers = { 'svls', 'rust_analyzer', 'clangd' }
 for _, lsp in pairs(servers) do
-  require('lspconfig')[lsp].setup {
+  lspconfig[lsp].setup {
     on_attach = on_attach,
-    flags = {
-      -- This will be the default in neovim 0.7+
-      debounce_text_changes = 150,
-    }
+    capabilities = capabilities,
   }
 end
+-- luasnip
+local luasnip = require 'luasnip'
+
+-- nvim-cmp
+local cmp = require 'cmp'
+cmp.setup {
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+  mapping = {
+    ['<C-p>'] = cmp.mapping.select_prev_item(),
+    ['<C-n>'] = cmp.mapping.select_next_item(),
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.close(),
+    ['<CR>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    },
+    ['<Tab>'] = function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end,
+    ['<S-Tab>'] = function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end,
+  },
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+  },
+}
 
 vim.g.better_whitespace_enabled = 1
 vim.g.strip_whitespace_on_save = 1
 
-vim.g.rainbow_active = 1
--- Colors
---vim.g.tokyonight_style = "storm"
---vim.cmd[[colorscheme tokyonight]]
-vim.cmd[[colorscheme jellybeans]]
-
+-- Comment <gc> to comment
+require('Comment').setup()
 -- lualine
 require('lualine').setup {
   options = {
@@ -86,3 +234,11 @@ require('lualine').setup {
   extensions = {}
 }
 
+-- Filetype specific settings
+local utils = require('utils')
+utils.create_augroup({
+  {'FileType', '*', 'setlocal', 'shiftwidth=2'},
+  {'FileType', 'rust', 'setlocal', 'shiftwidth=4'},
+  {'FileType', 'c,cpp', 'setlocal', 'shiftwidth=8'},
+  {'FileType', 'python', 'setlocal', 'shiftwidth=4'}
+}, 'Tab2')
